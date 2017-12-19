7288e7c208efc622dc5f94c7c611b69763fbb32d
wgzeyu <785777793@qq.com>
2017-12-19 17:38:51 +0800

    改为硬SPI连接，快了2ms
    软SPI接收串口+显示耗时共100ms
    硬SPI接收串口+显示耗时共98ms
    其中硬SPI显示部分耗时75ms

--- badappletest/badappletest.ino
+++ badappletest/badappletest.ino
@@ -1,6 +1,7 @@
 #include <U8g2lib.h>
 #include <U8x8lib.h>
-U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0,5,6,7,13);
+//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0,5,6,7,13); //软SPI比硬SPI在显示86*64位图时慢2ms
+U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0,10,8); //左到右 13 11 10(CS) 8(不用接)
 uint8_t apple[704] = {};
 void setup() {
   Serial.begin(115200);
